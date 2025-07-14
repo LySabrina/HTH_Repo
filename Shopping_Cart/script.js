7adf9674b08652c207c6cc236a5255fa42af8bde
lysabrina <92494792+LySabrina@users.noreply.github.com>
2025-07-14 13:08:53 -0700

    Fixed shopping cart js files

---
+++ Shopping_Cart/script.js
@@ -0,0 +1,69 @@
+// PRODUCT & CART DATA
+const products = {
+  apple: {
+    price: 2.0,
+    inventory: 2,
+  },
+
+  banana: {
+    price: 1.5,
+    inventory: 1,
+  },
+};
+
+const cart = [
+  ["apple", 2],
+  ["banana", 1],
+  ["banana", 2],
+];
+
+
+// FUNCTIONS FOR TERMINAL-BASED 
+/**
+ * Loop through cart items. Cart is represented as a nested tuples ( array inside array).
+ * For each item inside the cart ,check if that product exist and check if the inventory has enough amount
+ * @param {[[string, number]]} cart cart full of items to purchase
+ * @returns
+ */
+function totalCost(cart) {
+  let sum = 0;
+
+  for (const items in cart) {
+    const item = cart[items];
+    const itemName = item[0];
+    const itemAmount = item[1];
+
+    if (products[itemName]) {
+      if (products[itemName].inventory >= itemAmount) {
+        sum += products[itemName].price * itemAmount;
+        console.log("Purchasing - " + itemName + " Current Sum = " + sum);
+        updateInventory(itemName, products[itemName].inventory - itemAmount);
+      } else {
+        console.log(
+          `You are attempting to purchase, ${itemName} , more than what's available in stock`
+        );
+      }
+    } else {
+      console.log("Product " + itemName + " is out of stock");
+    }
+  }
+  return sum;
+}
+
+/**
+ * Updates the inventory of products. Deletes the products if inventory reaches 0 amount.
+ * @param {string} itemName name of the item
+ * @param {number} newAmount new inventory amount of the item
+ */
+function updateInventory(itemName, newAmount) {
+  if (newAmount === 0) {
+    delete products[itemName];
+  } else {
+    products[itemName][inventory] = newAmount;
+  }
+}
+
+// TESTING
+const total = totalCost(cart);
+console.log("Your Cart total: ", total);
+console.log("Current Products In Stock: ", products);
